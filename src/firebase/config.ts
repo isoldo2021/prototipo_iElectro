35fc4e873fa835ef36ec4ea884da5b879f25d8d4
isabel soldo <isusoldo@gmail.com>
2026-01-08 19:39:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5095798874-2c69f",
+  "appId": "1:339622832654:web:8342348cc7fa68e1c1ea5f",
+  "apiKey": "AIzaSyDMyTTiuY18htudZ3awgaGkfPHXgn8hfqk",
+  "authDomain": "studio-5095798874-2c69f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "339622832654"
+};

06f6aa3f0338b14825a013cb1acbbc48b92efe71
isabel soldo <isusoldo@gmail.com>
2026-01-08 20:30:40 +0000

    Consulta de pedido por mis pedidos y quitar debajo de la foro del usuari

--- src/components/user-auth.tsx
+++ src/components/user-auth.tsx
@@ -16,7 +16,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { LogOut, User as UserIcon, Package } from 'lucide-react';
+import { LogOut, User as UserIcon } from 'lucide-react';
 import { Skeleton } from './ui/skeleton';
 
 export function UserAuth() {
@@ -62,13 +62,6 @@ export function UserAuth() {
             </p>
           </div>
         </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-            <Link href="/orders">
-                <Package className="mr-2 h-4 w-4" />
-                <span>Mis Pedidos</span>
-            </Link>
-        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={handleSignOut}>
           <LogOut className="mr-2 h-4 w-4" />
